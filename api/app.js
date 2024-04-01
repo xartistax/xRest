f92168dcccca7e57bed0f3b451934b114e1d1868
Artista <demianfueglistaler@icloud..com>
2024-04-01 23:31:11 +0200

    added cors

--- api/app.js
+++ api/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const xDataRoutes = require('./xDataRoutes');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/xData', xDataRoutes); // Update the path as per your preference
